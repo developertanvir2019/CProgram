1460773775b9e6903e5971223f0a7625e1b72eb1
developertanvir2019 <developertanvir2019@gmail.com>
2024-03-31 23:07:38 +0600

    practice arethmetic oparator

---
+++ day2/arethmetic.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+// int main(){
+//     int a=10,b=20;
+//    int sum= a+b;
+// printf("%d",sum);
+//     return 0;
+// }
+
+
+// int main(){
+//     int a=10,b=20;
+//    int mul= a*b;
+// printf("%d",mul);
+//     return 0;
+// }
+
+
+int main(){
+    int a=10,b=3;
+   float devide= a*1.0/b;
+printf("%f",devide);
+    return 0;
+}
+
