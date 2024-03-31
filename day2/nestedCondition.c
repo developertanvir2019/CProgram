1af03f35f161b744bd54c596175ff8b20b5209f6
developertanvir2019 <developertanvir2019@gmail.com>
2024-04-01 00:12:32 +0600

    practic some quiz

--- day2/nestedCondition.c
+++ day2/nestedCondition.c
@@ -1,18 +1,57 @@
 #include <stdio.h>
 
-int main(){
-int tk=51100;
-if(tk>=5000){
-    printf("I will go cox bazar \n");
-    if(tk>=10000){
-        printf("I will go saint martin");
-    }else{
-        printf("came back from cox bazar");
+// int main(){
+// int tk=51100;
+// if(tk>=5000){
+//     printf("I will go cox bazar \n");
+//     if(tk>=10000){
+//         printf("I will go saint martin");
+//     }else{
+//         printf("came back from cox bazar");
+//     }
+
+// }else{
+//     printf("kothao jabo na");
+// }
+
+//     return 0;
+// }
+
+
+
+
+    // int main()
+    // {
+    //     int x = 5;
+    //     if (x < 10) {
+    //         printf("hello ");
+    //     }
+    //     if (x == 5) {
+    //         printf("hi");
+    //     }
+    //     else {
+    //         printf("no");
+    //     }
+    // }
+
+
+    //   int main()
+    // {
+    //     int x=0;
+    //     if (x=1) {
+    //         printf("hi");
+    //     }
+    //     else {
+    //         printf("how are u");
+    //     }
+    // }
+
+        int main()
+    {
+        if (4.10 > 5 || 5 > 10) {
+            printf("hi");
+        }
+        else {
+            printf("how are u");
+        }
     }
-
-}else{
-    printf("kothao jabo na");
-}
-
-    return 0;
-}
