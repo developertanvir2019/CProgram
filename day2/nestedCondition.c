76e5686338986ad03539863b77f60e07f5e1431c
developertanvir2019 <developertanvir2019@gmail.com>
2024-03-31 23:57:02 +0600

    nested condition practice

---
+++ day2/nestedCondition.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+int tk=51100;
+if(tk>=5000){
+    printf("I will go cox bazar \n");
+    if(tk>=10000){
+        printf("I will go saint martin");
+    }else{
+        printf("came back from cox bazar");
+    }
+
+}else{
+    printf("kothao jabo na");
+}
+
+    return 0;
+}
